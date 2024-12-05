4cf7503469801b7257336a1e547d66c5c0e97ff4
Katherine Eaton <ktmeaton@gmail.com>
2024-12-05 00:33:14 -0700

    checkpoint: before progress

--- myproject/gui.py
+++ myproject/gui.py
@@ -10,7 +10,9 @@ from workflow import Workflow, QueuingHandler
 
 from textual.app import App, ComposeResult
 from textual import work, events
-from textual.widgets import Header, Footer, Static, RichLog, Tree
+from textual.widgets import Header, Footer, Static, RichLog, Tree, Label
+from textual.widgets.tree import UnknownNodeID
+
 
 class TaskTree(Tree):
     DEFAULT_CSS = """
@@ -37,8 +39,15 @@ class Resources(Static):
         column-span: 1;
         height: 100%;
         border: solid blue;
+        border-title-align: center;
+        padding: 1 1;
     }
     """
+    BORDER_TITLE = "Resources"
+
+    def compose(self) -> ComposeResult:
+        yield Label("Threads:")
+        yield Label("Memory:")
 
 class Log(RichLog):
     DEFAULT_CSS = """
@@ -85,13 +94,15 @@ class Gui(App):
         self.footer = Footer()
         yield self.footer
 
+        self.task_tree_lookup = {}
         self.task_tree = TaskTree("Tasks")
+        self.task_tree.root.expand()
         yield self.task_tree
     
         self.progress = Progress("Progress")
         yield self.progress
     
-        self.resources = Resources("Resources")
+        self.resources = Resources()
         yield self.resources
 
         self.run_log = Log(highlight=True, markup=True)
@@ -102,77 +113,99 @@ class Gui(App):
         self.subtitle = "subtitle"
 
         self.create_logger()
-        self.logger.info("GUI loaded.")
-
         self.gui = self.set_interval(1 / self.fps, self.update_gui)
         self.gui.resume()
-        # self.load_workflow()
-        # self.run_workflow()
+
+        await self.load_workflow()
+        self.run_workflow()
 
 
     def create_logger(self) -> None:
-        LOG_FORMAT = '%(asctime)s %(name)6s %(levelname)8s: %(message)s'
-        #  Setup root logger to write to a log file.
+        #log_format = '%(asctime)s %(name)6s %(levelname)8s: %(message)s'
+        log_format = '%(asctime)s %(funcName)20s %(levelname)8s: %(message)s'
+        #  Root logger
         logging.basicConfig(
-            format=LOG_FORMAT,
+            format=log_format,
             level=logging.DEBUG,
             datefmt='%Y-%m-%d %H:%M:%S',
             filename=self.logfile,
             filemode='w'
         )
-
-        #  Get a child logger
-        self.logger = logging.getLogger(name='gui')
-
-        #  Build our QueuingHandler
+        # Child logger
+        self.logger = logging.getLogger(name="gui")
         handler = QueuingHandler(message_queue=self.message_queue, level=logging.DEBUG)
-
-        #  Change the date/time format for the GUI to drop the date
-        formatter = logging.Formatter(LOG_FORMAT)
-        formatter.default_time_format = '%H:%M:%S'
+        formatter = logging.Formatter(log_format)
         handler.setFormatter(formatter)
-
-        #  Add our QueuingHandler into the logging heirarchy at the lower level
         self.logger.addHandler(handler)
-        self.logger.info("INFO in gui")
-        self.logger.debug("DEBUG in gui")
+        self.logger.debug("Logger ready.")
 
     @work
     async def update_gui(self) -> None:
-        # Update log
-        message = self.message_queue.get()
-        #message_queue, self.message_queue = list(self.message_queue), queue.Queue()
-        # for message in message_queue:
-        #     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #     self.run_log.write(f"{now} [INFO] {message}")
+        self.update_log()
+        self.update_tree()
+        # TODO: self.update_progress()
+        # TODO: self.update_resources()
 
+    @work
+    async def update_log(self):
+        if not self.message_queue.empty():
+            message = self.message_queue.get()
+            self.run_log.write(message)
 
-    def load_workflow(self) -> None:
-        #self.message_queue.append(f"Loading workflow: {self.workflow_yaml}")
+    @work
+    async def update_tree(self):
+        for job in self.workflow.jobs:
+            identifier = job
+            if job not in self.task_tree_lookup:
+                job_node = self.task_tree.root.add(job, expand=True)
+                self.task_tree_lookup[identifier] = job_node
+            else:
+                job_node = self.task_tree_lookup[job]
+            for step in self.workflow.get_steps(job):
+                identifier = f"{job}.{step}"
+                if identifier not in self.task_tree_lookup:
+                    step_node = job_node.add(step, expand=True)
+                    self.task_tree_lookup[identifier] = step_node 
+                else:
+                    step_node = self.task_tree_lookup[identifier]
+                for task in self.workflow.get_tasks(job, step):
+                    identifier = f"{job}.{step}.{task}"
+                    if identifier not in self.task_tree_lookup: 
+                        task_label = "{:10} {:5}".format(task, "⏩")
+                        task_node = step_node.add_leaf(task_label)
+                        self.task_tree_lookup[identifier] = task_node 
+                    else:
+                        task_node = self.task_tree_lookup[identifier]
+
+    async def load_workflow(self) -> None:
+        self.logger.info(f"Loading workflow: {self.workflow_yaml}")
         self.workflow = Workflow(self.workflow_yaml)
-        #self.messages.append(f"Workflow loaded: {self.workflow.name}")
+        self.logger.info(f"Workflow loaded: {self.workflow.name}")
         self.title = self.workflow.name
-        #self.messages.append(self.workflow.jobs)
-        #self.task_tree.root.add("test")
-        #self.task_tree.add_json(self.workflow.jobs)
 
 
     @work(thread=True)
     async def run_workflow(self) -> None:
-        self.messages.append(f"Beginning workflow.")
-        tasks = []
-        for job in self.workflow.jobs:
-            self.messages.append(f"Starting job: {job}")
-            tasks = await self.workflow.run_job(job)
-            for task in asyncio.as_completed(tasks):
-                result = await task
-                self.messages.append(f"task: {result}")
-            #     #await task
-            # # #     self.messages.append(messages)
-        self.messages.append(f"Completed workflow.")
+        tasks = self.workflow.run_workflow(log_name='gui')
+        # for task in asyncio.as_completed(self.workflow.task_queue):
+        #     self.logger.info(f"Waiting for task: {task}")
+        #     result = await task
+        #     self.logger.info(f"Completed task: {result}")
+        #for task in asyncio.as_completed(tasks): 
+        #   result = await task
+        #self.logger.info("Workflow complete.")
+        # self.messages.append(f"Beginning workflow.")
+        # tasks = []
+        # for job in self.workflow.jobs:
+        #     self.messages.append(f"Starting job: {job}")
+        #     tasks = await self.workflow.run_job(job)
+        #     for task in asyncio.as_completed(tasks):
+        #         result = await task
+        #         self.messages.append(f"task: {result}")
+        #     #     #await task
+        #     # # #     self.messages.append(messages)
 
 
     def on_key(self, event: events.Key) -> None:
         self.logger.debug(event)
-        #self.messages.append(event)
     
--- myproject/workflow.py
+++ myproject/workflow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python3
 
 import asyncio
+from collections import OrderedDict
 import yaml
 import time
 from typing import List
@@ -8,45 +9,75 @@ from typing import List
 import logging
 import queue
 
-class Step:
-    def __init__(self, name, id):
-        self.name = name
-        self.id   = id
+# class Step:
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id   = id
 
-class TaskResult:
-    def __init__(self, identifier):
-        self.id  = identifier
-    def __repr__(self):
-        return f"{self.id}"
+# class TaskResult:
+#     def __init__(self, identifier):
+#         self.id  = identifier
+#     def __repr__(self):
+#         return f"{self.id}"
 
 class Workflow:
     def __init__(self, path:str):
         """Create a Workflow based on a YAML path"""
         self.path = path
+        self.tree = OrderedDict()
+        self.tasks = OrderedDict()
 
         with open(path) as infile:
             self.data = yaml.safe_load(infile)
         self.name = self.data["name"] if "name" in self.data else "unknown"
         self.jobs = self.data["jobs"] if "jobs" in self.data else {}
-        
+        self.logger = logging.getLogger(name='workflow')
 
-    async def run_job(self, job) -> List[asyncio.Task]:
-        job_data = self.jobs[job]
-        tasks = []
-        if job_data == None or "steps" not in job_data and job_data["steps"] != None:
-            return tasks
-        else:
-            for step,step_data in job_data["steps"].items():
-                task = asyncio.create_task(self.demo(f"{job}.{step}"))
-                tasks.append(task)
-            return tasks
+
+    def run_workflow(self, log_name: str):
+        """Run workflow and add tasks to the queue."""
+        logger = logging.getLogger(name=log_name)
+        logger.info("Running workflow.")
+        for job,job_data in self.jobs.items():
+            self.validate_job(job, logger)
+            self.tree[job] = OrderedDict()
+            # steps = job_data["steps"] if "steps" in job_data and job_data["steps"] != None else {}
+            # for step,step_data in job_data["steps"].items():
+            #     logger.info(f"Running step: {step}")
+            #     if step in self.tree[job]:
+            #         msg = "Step name {step} is not unique to job {job}."
+            #         logger.error(msg)
+            #         raise Exception(msg)
+            #     for i,task in enumerate(step_data):
+            #         identifier = f"{job}.{step}.{task}.{i}"
+            #         if task in self.tree[job][step]:
+            #             msg = "Task name {task} is not unique to step {step}."
+            #             logger.error(msg)
+            #             raise Exception(msg)
+            #         logger.info(f"Dispatching task: {identifier}")
+            #         t = asyncio.create_task(self.demo(identifier))
+            #         self.tasks[identifier] = t
+
+    def validate_job(self, job, logger):
+        logger.info(f"Validating job: {job}")
+        if job in self.tree:
+            msg = "Job name '{job}' is not unique."
+            logger.error(msg)
+            raise Exception(msg)
+
+    def get_steps(self, job) -> dict:
+        d = self.jobs[job]
+        return d["steps"] if "steps" in d and d["steps"] != None else {}
+
+    def get_tasks(self, job, step) -> dict:
+        return self.jobs[job]["steps"][step]
+
 
 
     async def demo(self, identifier):
-        if step == "slow":
+        if "slow" in identifier:
            time.sleep(5)
-
-        return TaskResult(identifier)
+        return identifier
 
 class QueuingHandler(logging.Handler):
     """
